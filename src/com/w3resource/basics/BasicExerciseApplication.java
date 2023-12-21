9e28a1b7029134d570ec5e4a34860c65350f97e7
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2023-12-21 15:04:24 -0800

    mad e through 22 exercises

--- src/com/w3resource/basics/BasicExerciseApplication.java
+++ src/com/w3resource/basics/BasicExerciseApplication.java
@@ -4,12 +4,75 @@ public class BasicExerciseApplication {
 
 	public static void main(String[] args) {
 		ExercisesPractice1 practice = new ExercisesPractice1();
+
+//		System.out.println("--> Ex. 1");
+//		practice.exercise1("Jeff");
+//		
+//		System.out.println("--> Ex. 2");
+//		practice.exercise2(74, 36);
+//		
+//		System.out.println("--> Ex. 3");
+//		practice.exercise3(50, 3);
+//		
+//		System.out.println("--> Ex. 4");
+//		practice.exercise4();
+//		
+//		System.out.println("--> Ex. 5");
+//		practice.exercise5(25, 5);
+//		
+//		System.out.println("--> Ex. 6");
+//		practice.exercise6(125, 24);
+//		
+//		System.out.println("--> Ex. 7");
+//		practice.exercise7();
+
+//		System.out.println("--> Ex. 8");
+//		practice.exercise8();
+//
+//		System.out.println("--> Ex. 9");
+//		practice.exercise9();
+//
+//		System.out.println("--> Ex. 10");
+//		practice.exercise10();
+
+//		System.out.println("--> Ex. 11");
+//		practice.exercise11();
+//
+//		System.out.println("--> Ex. 12");
+//		practice.exercise12();
+
+//		System.out.println("--> Ex. 13");
+//		practice.exercise13();
+
+//		System.out.println("--> Ex. 14");
+//		practice.exercise14();
+
+//		System.out.println("--> Ex. 15");
+//		practice.exercise15();
+//		
+//		System.out.println("--> Ex. 16");
+//		practice.exercise16();
+
+//		System.out.println("--> Ex. 17"); // DID NOT FINISH
+//		practice.exercise17();
 		
-		practice.exercise1("Jeff");
+//		System.out.println("--> Ex. 31"); 
+//		practice.exercise31();
+//	
 		
-		practice.exercise2(74, 36);
-		
-		practice.exercise3(50, 3);
+//		System.out.println("--> Ex. 32"); 
+//		practice.exercise32();
+	
+//		System.out.println("--> Ex. 33"); 
+//		practice.exercise33();
+	
+//		System.out.println("--> Ex. 37"); 
+//		practice.exercise37();
+	
+//		System.out.println("--> Ex. 38"); ********GOODONE
+//		practice.exercise38();
+	
+
 	}
 
 }
